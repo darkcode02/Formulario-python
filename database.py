f7987890e326a5ed9e58c0c0c6b1a7e0c61ac956
Juan Pablo <juan.pablo.arias.0204@gmail.com>
2024-08-12 22:09:10 -0500

    Cambios en toda la estructura del proyecto

--- database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "mysql+mysqlconnector://root:root_password@mysql-container/mydatabase"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base: DeclarativeMeta = declarative_base()
